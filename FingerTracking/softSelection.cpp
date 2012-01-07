86faf476ef0f3f77fbacfabe383b98766a6d1ee9
chantree chandrapunth <cchandrapunth@gmail.com>
2012-01-06 23:08:44 -0500

    Update softselection

--- FingerTracking/softSelection.cpp
+++ FingerTracking/softSelection.cpp
@@ -1,23 +1,54 @@
 #include "stdafx.h"
-
+#include <math.h>
 #include "model.h"
 #include "softSelection.h"
 
-void softSelect(model_t* model, int id, point_t* vertexlist,float transx, float transy, float* pointt){
+point_t* vlist; 
+float* plist;
+int maxEffect =200;
 
-	polygon_t p= model->pList[id];
-	//translate x and y of the points in the chosen poly
-	for(int k=0; k<4; k++){
-		int index = p.p[k];
-		
-		float prevx = vertexlist->pPoints[index].X;
-		float prevy = vertexlist->pPoints[index].Y;
+void translatePoint(int id, float mult, float transx, float transy){
+		float prevx = vlist->pPoints[id].X;
+		float prevy = vlist->pPoints[id].Y;
 
-		vertexlist->pPoints[index].X = prevx+transx;
-		vertexlist->pPoints[index].Y = prevy+transy;
-		
+		vlist->pPoints[id].X = prevx+(mult*transx);
+		vlist->pPoints[id].Y = prevy+(mult*transy);
+
 		//index indecates points 
-		pointt[index*3] = prevx+transx;	//x pointdinate
-		pointt[index*3+1] = prevy+transy; //y pointdinate
-	}
+		plist[id*3] = prevx+(mult*transx);	//x pointdinate
+		plist[id*3+1] = prevy+(mult*transy); //y pointdinate
 }
+
+//assign max distance = 500
+void linearfunc(float xc, float yc, float transx, float transy){
+	int n = vlist->nVertexs;
+	
+	for(int i=0; i<n; i++){
+		float dis = sqrt(pow(vlist->pPoints[i].X -xc, 2)+ pow(vlist->pPoints[i].Y -yc, 2));
+		if(dis < maxEffect){
+			double param = (maxEffect-dis)/maxEffect;
+			translatePoint(i,pow(param, 2), transx, transy);
+		}
+	}
+
+}
+
+
+void softSelect(model_t* model, int id, point_t* vertextlist,float transx, float transy, float* pointt){
+
+	vlist = vertextlist;
+	plist = pointt;
+
+	//acquire the mesh
+	polygon_t mesh= model->pList[id];
+	
+	//get its vertex id
+	int* targets = mesh.p;
+	
+	//find vertex center of the mass (estimated)
+	float xCenter = (vlist->pPoints[targets[0]].X + vlist->pPoints[targets[2]].X)/2;
+	float yCenter = (vlist->pPoints[targets[1]].Y + vlist->pPoints[targets[3]].Y)/2;
+
+	linearfunc(xCenter, yCenter, transx, transy);
+}
+
